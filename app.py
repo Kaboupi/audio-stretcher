6c2706804c1a5e577d15bacc8edbc29a242cd339
kaboupi <gnovozhiloff@gmail.com>
2023-03-01 00:40:04 +0300

    initial commit Стадия анализа, hop size

---
+++ app.py
@@ -0,0 +1,7 @@
+import scipy.io
+from scipy.io import wavfile
+from os.path import dirname, join as pjoin
+from crud import load_wav
+
+if __name__ == '__main__':
+    load_wav()
---
+++ src/crud.py
@@ -0,0 +1,8 @@
+"""
+simple crud.py script
+"""
+from scipy.io import wavfile
+from os.path import dirname, join as pjoin
+
+def load_wav():
+    return
