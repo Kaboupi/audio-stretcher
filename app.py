211e9f84314fefef1503dad8c17be6aa7c11a122
kaboupi <gnovozhiloff@gmail.com>
2023-03-06 00:01:48 +0300

    Add stereo file support and fixed the ratio

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ import sys
 import scipy.io
 from scipy.io import wavfile
 from src.compute import pitchShift
+import numpy as np
 
 # ./run.sh "C:\PythonProjects\repos\phase_vocoder\input\test_mono.wav" "C:\PythonProjects\repos\phase_vocoder\output\test_mono_output.wav" 2.56
-
+# ./run.sh "C:\PythonProjects\repos\phase_vocoder\input\test_stereo.wav" "C:\PythonProjects\repos\phase_vocoder\output\test_stereo_output.wav" 2
 def load_args():
     try:
         input_arg, output_arg, shift_scale = sys.argv[1:4]
@@ -14,12 +15,17 @@ def load_args():
 
 if __name__ == '__main__':
     input_str, output_str, shift_scale = load_args()  # Загрузили аргументы баш скрипта
+    if shift_scale > 0 and shift_scale < 1:
+        shift_scale = -1 / shift_scale
+    shift_scale *= 6
+    
     samplerate, data = wavfile.read(input_str)  # Записали частоту дискретизации и данные в samplerate, data соответственно
     
-    if shift_scale > 0 and shift_scale < 1:
-        shift_scale *= -4
-        
-    y = pitchShift(data, step=shift_scale * 6)
+    try:
+        y_left = pitchShift(data[:,0], step=shift_scale)
+        y_right = pitchShift(data[:,1], step=shift_scale)
+        y = np.array([y_left, y_right]).T
+    except IndexError:
+        y = pitchShift(data, step=shift_scale)
     
     wavfile.write(output_str, samplerate, y)
-    
