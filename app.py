15d333c6cd5590614b6eb90f44cec2650f454cf8
kaboupi <gnovozhiloff@gmail.com>
2023-03-05 21:26:31 +0300

    Final commit

--- app.py
+++ app.py
@@ -1,30 +1,25 @@
 import sys
 import scipy.io
 from scipy.io import wavfile
-from os.path import dirname, join as pjoin
-from src.crud import load_wav, save_wav, plot_wav_freq
-from src.compute import analysis, phase_shift, synthesation, set_params
+from src.compute import pitchShift
 
 # ./run.sh "C:\PythonProjects\repos\phase_vocoder\input\test_mono.wav" "C:\PythonProjects\repos\phase_vocoder\output\test_mono_output.wav" 2.56
 
 def load_args():
     try:
         input_arg, output_arg, shift_scale = sys.argv[1:4]
-        return input_arg, output_arg, float(shift_scale)
+        return input_arg, output_arg, abs(float(shift_scale))
     except ValueError:
         print('Incorrect number of arguments!')
 
 if __name__ == '__main__':
     input_str, output_str, shift_scale = load_args()  # Загрузили аргументы баш скрипта
-    samplerate, data = load_wav(input_str)  # Записали частоту дискретизации и данные в samplerate, data соответственно
+    samplerate, data = wavfile.read(input_str)  # Записали частоту дискретизации и данные в samplerate, data соответственно
     
-    set_params(frame_size=int(data.shape[0] // 1000))
+    if shift_scale > 0 and shift_scale < 1:
+        shift_scale *= -4
+        
+    y = pitchShift(data, step=shift_scale * 6)
     
-    spectrum, frequencies = analysis(data, samplerate)  # Спектр распределения аудиочастот и на каждый тик
-    phase = phase_shift(data=spectrum, samplerate=samplerate, frequencies=frequencies)  # Сдвинутые на 1 фазу значения (не нормализованные)
-    
-    y_n = synthesation(data=phase, frequencies=frequencies)  # TODO ДОЛЖЕН БЫТЬ ИТОГОВЫЙ ФРЕЙМ
-    
-    
-    save_wav(output_str=output_str, samplerate=int(samplerate * shift_scale), data=data[:5*samplerate])
+    wavfile.write(output_str, samplerate, y)
     
--- src/compute.py
+++ src/compute.py
@@ -1,96 +1,75 @@
 import numpy as np
-from scipy.fft import fft
+from scipy.fft import fft, ifft
 from scipy.signal.windows import hann
-from typing import List, Optional
+from scipy.interpolate import interp1d
 
-FRAME_SIZE = 1000
-OVERLAP = 0.75
-HOP_A = int(FRAME_SIZE - (FRAME_SIZE * OVERLAP))
-
-def set_params(frame_size: int, overlap: Optional[float] = 0.75):
-    FRAME_SIZE = frame_size
-    OVELRAP = overlap
-    HOP_A = int(FRAME_SIZE - (FRAME_SIZE * OVERLAP))
-
-# 1st ANALYSIS
-def analysis(data: np.ndarray,
-             samplerate: int,
-             frame_size: int = FRAME_SIZE,
-             overlap: float = OVERLAP,
-             ) -> List[np.ndarray]:
-    """
-    Compute the discrete spectrum of a signal.
-
-    Args:
-        data: numpy array of shape (n,)
-            The input signal to compute the spectrum for.
-        frame_size: int, optional (default=n//1000)
-            The size of each frame, in samples.
-        overlap: float, optional (default=0.75)
-            The overlap (hop size) of frames.
-
-    Returns:
-        spectrum: list of numpy arrays shaped (frame_size,)
-            The spectral amplitude at each frequency bin for each frame.
-    """
-    hop_size = int(frame_size - (frame_size * overlap))
-    num_frames = 1 + (data.shape[0] - frame_size) // hop_size
-    hann_window = hann(frame_size)
-    spectrum = np.zeros((frame_size // 2 + 1, num_frames))
+def createFrames(x: np.ndarray, hop_size: int = 256, frame_size: int = 1024):
+    number_slices = (x.shape[0] - frame_size) // hop_size
+    x = x[:int((number_slices * hop_size + frame_size))]
     
-    for i in range(num_frames):
-        frame = data[i*hop_size:i*hop_size+frame_size] * hann_window
-        spectrum[:, i] = (np.abs(fft(frame)[:frame_size // 2 + 1]))
-        
-    frequencies = np.fft.fftfreq(frame_size, 1/samplerate)[:frame_size//2 + 1]
-        
-    return spectrum, frequencies
-
-# 2nd PHASE SHIFT
-def phase_shift(data: np.ndarray,
-                samplerate: int,
-                frequencies: np.ndarray,
-                frame_size: int = FRAME_SIZE,
-                hop_size: int = HOP_A,
-                ) -> np.ndarray:
-    """
-    Compute phase shift.
-    """
-    num_frames = data.shape[1]
-    w_true = w_freq = w_wrapped = np.zeros((frame_size // 2 + 1, num_frames))
+    vector_frames = np.zeros((int(x.shape[0] / hop_size), frame_size))
     
-    for i in range(1, num_frames):
-        w_freq[:, i-1] = (data[:, i] - data[:, i-1]) / (hop_size / samplerate) - frequencies
-        w_wrapped[:, i-1] = (w_freq[:, i-1] + np.pi) % (2 * np.pi) - np.pi
-        w_true[:, i-1] = w_wrapped[:, i-1] + frequencies
+    for i in range(1, number_slices):
+        idx_begin = (i - 1) * hop_size
+        idx_end = (i - 1) * hop_size + frame_size
         
-    phase = np.zeros((frame_size // 2 + 1, num_frames))
-    hop_s = int(data.shape[0] - (data.shape[0] * OVERLAP))
-    for i in range(1, num_frames):
-        phase[:, i] = data[:, i-1] + (hop_s / samplerate) * w_true[:, i]
-
-    return phase
-
-
-# 3rd SYNTHESIS
-def synthesation(data: np.ndarray,
-                 frequencies: np.ndarray,
-                 frame_size: int = FRAME_SIZE,
-                 hop_size: int = HOP_A
-                 ) -> None:
-    """
-    ???
-    """
-    hop_s = int(data.shape[0] - (data.shape[0] * OVERLAP))
-    num_frames = data.shape[1]
-    hann_window = hann(data.shape[0])
-    Q_i = np.zeros((data.shape[0], num_frames))
+        vector_frames[i,:] = x[idx_begin:idx_end]
     
-    # for i in range(num_frames):
-    #     Q_i[:, i] = np.mean(fft(np.abs(data[:, i]))) * hann_window
+    return vector_frames, number_slices
+
+
+def fusionFrames(frame_matrix, hop_size: int = 256):
+    number_frames, size_frames = frame_matrix.shape
+    
+    vectorTime = np.zeros(number_frames * hop_size - hop_size + size_frames)
+    timeIndex = 0
+    
+    for i in range(number_frames):
+        vectorTime[timeIndex:timeIndex + size_frames] += frame_matrix[i,:]
+        timeIndex += hop_size
         
-    res = np.zeros((data.shape[0], num_frames))
+    return vectorTime
+
+
+def pitchShift(data: np.ndarray, frame_size: int = 1024, hop_size: int = 256, step: int = 2) -> np.ndarray:
+    alpha = np.power(2, step / 12)
+    
+    hopOut = np.round(alpha * hop_size)
+    wn = hann(frame_size*2 + 1)[2::2]
+    
+    # Source file
+    data = np.concatenate([np.zeros(hop_size * 3), data])
+    
+    #
+    y, numberFramesInput = createFrames(data)
+    numberFramesOutput = numberFramesInput
+    outputy = np.zeros((numberFramesOutput, frame_size))
+    
+    phaseCumulative = 0
+    previousPhase = 0
+    
+    for i in range(numberFramesInput):
         
-    return res
+        currentFrame = y[i,:]
+        currentFrameWindowed = currentFrame * wn / np.sqrt((frame_size / hop_size) / 2)
+        currentFrameWindowedFFT = fft(currentFrameWindowed)
+        magFrame = np.abs(currentFrameWindowedFFT)
+        phaseFrame = np.angle(currentFrameWindowedFFT)
         
-    # frame = data[i*hop_size:i*hop_size+frame_size] * hann_window
+        # Обработка
+        deltaPhi = phaseFrame - previousPhase
+        previousPhase = phaseFrame
+        
+        deltaPhiPrime = deltaPhi - hop_size * 2 * np.pi * np.arange(frame_size) / frame_size
+        deltaPhiPrimeMod = np.mod((deltaPhiPrime + np.pi), (2 * np.pi)) - np.pi
+        
+        trueFreq = 2 * np.pi * np.arange(frame_size) / frame_size + deltaPhiPrimeMod / hop_size
+        phaseCumulative += hopOut * trueFreq
+        
+        # Синтез
+        outputMag = magFrame
+        outputFrame = np.real(ifft(outputMag * np.exp(1j * phaseCumulative)))
+        
+        outputy[i,:] = outputFrame * wn / np.sqrt((frame_size / hopOut) / 2)
+    
+    return fusionFrames(outputy, int(hopOut)).astype(np.int16)
